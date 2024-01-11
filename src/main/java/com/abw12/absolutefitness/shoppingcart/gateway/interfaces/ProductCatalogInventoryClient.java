a937c411cd722a42c5918c513be5ceb276104626
Abhishek Wasave <Abhi_wasave@abw12>
2024-01-11 18:12:14 +0530

    AddToCart API changes and few configuration added

--- src/main/java/com/abw12/absolutefitness/shoppingcart/gateway/interfaces/ProductCatalogInventoryClient.java
+++ src/main/java/com/abw12/absolutefitness/shoppingcart/gateway/interfaces/ProductCatalogInventoryClient.java
@@ -1,17 +1,15 @@
 package com.abw12.absolutefitness.shoppingcart.gateway.interfaces;
 
-import com.abw12.absolutefitness.shoppingcart.dto.request.InventoryValidationReq;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
-import java.util.Objects;
 
 @FeignClient(name ="product-inventory-controller")
 public interface ProductCatalogInventoryClient {
 
     @GetMapping("/checkStockStatus")
-    ResponseEntity<Map<String, Objects>> cartValidation(@RequestBody InventoryValidationReq request);
+    ResponseEntity<Map<String, Object>> cartValidation(@RequestParam Map<String,String> request);
 }
--- src/main/java/com/abw12/absolutefitness/shoppingcart/repository/CartItemRepository.java
+++ src/main/java/com/abw12/absolutefitness/shoppingcart/repository/CartItemRepository.java
@@ -1,11 +1,14 @@
 package com.abw12.absolutefitness.shoppingcart.repository;
 
 import com.abw12.absolutefitness.shoppingcart.entity.CartItemDAO;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,4 +21,15 @@ public interface CartItemRepository extends JpaRepository<CartItemDAO,String> {
     @Modifying
     @Query("DELETE FROM CartItemDAO i WHERE i.cartId =:cartId")
     Optional<Integer> deleteCartItemByCartId(String cartId);
+    @Query("SELECT i FROM CartItemDAO i WHERE i.variantId =:variantId")
+    Optional<CartItemDAO> findByVariantId(String variantId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE CartItemDAO c SET c.cartItemQuantity = :newCartItemQuantity, c.cartItemModifiedAt = :newCartItemModifiedAt WHERE c.cartItemId = :cartItemId")
+    int updateCartItem(
+            @Param("cartItemId") String cartItemId,
+            @Param("newCartItemQuantity") Long newCartItemQuantity,
+            @Param("newCartItemModifiedAt") OffsetDateTime newCartItemModifiedAt
+    );
 }
